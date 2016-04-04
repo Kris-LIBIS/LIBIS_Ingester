166b2e4093ea81d65696cb3e14f66412f792ccc4
Kris Dekeyser <kris.dekeyser@libis.be>
2016-04-04 15:44:23 +0200

    New Collective Access exporter task

---
+++ lib/libis/ingester/tasks/collective_access_exporter.rb
@@ -0,0 +1,44 @@
+require 'fileutils'
+
+require 'libis/tools/xml_document'
+
+require 'libis-ingester'
+require 'libis/ingester/ftps_service'
+
+module Libis
+  module Ingester
+
+    class CollectiveAccessExporter < ::Libis::Ingester::Task
+
+      parameter export_dir: '.', description: 'Directory where the export files will be copied'
+      parameter item_types: ['Libis::Ingester::IntellectualEntity']
+      parameter recursive: true, frozen: true
+
+      protected
+
+      def process(item)
+        export_item(item)
+        stop_processing_subitems
+      end
+
+      private
+
+      # @param [Libis::Ingester::IntellectualEntity] item
+      def export_item(item)
+        identifier = item.properties['identifier']
+        unless item.pid
+          warn "Object #{identifier} was not ingested fully."
+          return
+        end
+
+        FileUtils.mkdir_p(parameter(:export_dir))
+        export_file = File.join(parameter(:export_dir), "#{item.get_run.name}.csv")
+        open(export_file, 'a') do |f|
+          f.puts "#{item.name}\t#{item.pid}"
+        end
+
+      end
+
+    end
+  end
+end
