3a11483736b9bdbc2ff4c826fda0e65e34104a0b
Kris Dekeyser <kris.dekeyser@libis.be>
2016-04-05 19:44:21 +0200

    Fix commong sidekiq config

--- bin/sidekiq.config.rb
+++ bin/sidekiq.config.rb
@@ -3,7 +3,7 @@ require 'sidekiq/api'
 Sidekiq.configure_client do |config|
   # noinspection RubyResolve
   config.redis = {
-      url: installer.config.sidekiq.redis_url,
+      url: @installer.config.sidekiq.redis_url,
       namespace: (installer.config.sidekiq.namespace rescue 'Ingester'),
   }
 end
