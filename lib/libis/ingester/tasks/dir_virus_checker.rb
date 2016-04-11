03c5336ebb28820ada86d6a4505a91315170d417
Kris Dekeyser <kris.dekeyser@libis.be>
2016-04-11 18:46:30 +0200

    Added new task to scan directory i.o. individual files.

---
+++ lib/libis/ingester/tasks/dir_virus_checker.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+require 'libis/ingester'
+require 'libis-tools'
+
+module Libis
+  module Ingester
+
+      class DirVirusChecker < ::Libis::Ingester::Task
+
+        parameter location: '.',
+                  description: 'Directory to scan for viruses'
+
+        parameter item_types: [Libis::Ingester::Run], frozen: true
+
+        def pre_process(item)
+          super
+          skip_processing_item if item.options['virus_checked']
+        end
+
+        def process(item)
+
+          raise Libis::Workflow::AbortError, "Location does not exist: #{parameter(:location)}." unless Dir.exists?(parameter(:location))
+
+          debug 'Scanning directory %s for viruses', parameter(:location)
+
+          # noinspection RubyResolve
+          cmd_options = Libis::Ingester::Config.virusscanner['options'] + '-r'
+          # noinspection RubyResolve
+          result = Libis::Tools::Command.run Libis::Ingester::Config.virusscanner[:command], *cmd_options, parameter(:location)
+          raise Libis::WorkflowError, "Error during viruscheck: #{result[:err]}" unless result[:status]
+
+          item.options['virus_checked'] = true
+          debug 'Directory is clean'
+
+        end
+
+      end
+
+  end
+end
