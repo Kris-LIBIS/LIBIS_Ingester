b5566929e1a90b1a212a42befd9b74d5b07cf545
Kris Dekeyser <kris.dekeyser@libis.be>
2016-05-02 15:59:56 +0200

    Added status check

---
+++ bin/status.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+require_relative '../lib/libis/ingester/console/include'
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  common_opts(opts)
+  run_opts(opts)
+
+end.parse!
+
+get_initializer
+
+loop do
+  break unless select_user
+  loop do
+    break unless select_organization
+    loop do
+      break unless select_job
+      loop do
+        @options[:job].reload_relations
+        break unless select_run
+        format_str = '%-30s %-20s %-20s %s'
+        puts format_str % %w'Task Started Updated Progress'
+        puts '-' * 90
+        @options[:run].status_log.each do |status|
+          task = status['task'].gsub(/[^\/]*\//,' - ')
+          data = [
+              task,
+              status['created'].strftime('%d/%m/%Y %T'),
+              status['updated'].strftime('%d/%m/%Y %T'),
+              status['status'].to_s.capitalize
+          ]
+          if status['status'].to_s != 'DONE' && status['progress']
+            x = status['progress'].to_s
+            x += ' of ' + status['max'].to_s if status['max']
+            data[4] = x
+          end
+          puts format_str % data
+        end
+        @options[:run] = nil
+      end
+      @options[:job] = nil
+    end
+    @options[:organization] = nil
+  end
+  exit
+end
+exit
