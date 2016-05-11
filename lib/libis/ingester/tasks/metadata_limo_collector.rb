669b9f9472cac71138929d232469e04ac7915911
Kris Dekeyser <kris.dekeyser@libis.be>
2016-05-11 18:27:48 +0200

    Limo Collector: improve exception handling

--- lib/libis/ingester/tasks/metadata_limo_collector.rb
+++ lib/libis/ingester/tasks/metadata_limo_collector.rb
@@ -20,8 +20,7 @@ module Libis
         @limo ||= Libis::Services::Primo::Limo.new(parameter(:host))
 
         result = @limo.get_marc(term)
-        raise Exception, "#{result[:error_type]} - #{result[:error_name]}" if result.is_a?(Hash)
-        result = result.root rescue nil
+        result = result.root
 
         return result.blank? ? nil : Libis::Tools::Metadata::Marc21Record.new(result)
 
