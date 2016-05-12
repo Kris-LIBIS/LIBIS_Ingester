daf189354aae1412ebdae87fb2c494b8b6ec9590
Kris Dekeyser <kris.dekeyser@libis.be>
2016-05-12 16:22:47 +0200

    queue: add verification message

--- bin/queue.rb
+++ bin/queue.rb
@@ -13,8 +13,8 @@ loop do
   break unless (queue = select_defined_queue(with_create: true, with_delete: true))
   loop do
     break unless (worker = select_worker(queue))
+    worker_name = "#{worker.klass.constantize.subject(worker.args.first).name} #{worker.args.last}"
+    break unless @hl.agree("About to delete worker #{worker_name} from the queue. OK?", false)
     worker.delete
   end
 end
-
-# Sidekiq::Stats.new.reset
