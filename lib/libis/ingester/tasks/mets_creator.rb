5783dccf082ce4d87557367319ba808b635790f3
Kris Dekeyser <kris.dekeyser@libis.be>
2016-06-20 19:25:14 +0200

    MetsCreator: make sure dc:title is always set

--- lib/libis/ingester/tasks/mets_creator.rb
+++ lib/libis/ingester/tasks/mets_creator.rb
@@ -65,7 +65,7 @@ module Libis
                       Libis::Tools::Metadata::DublinCoreRecord.new
                     end
 
-        dc_record.title = item.label if item.label != item.name
+        dc_record.title ||= item.label
 
         collection_list = item.ancestors.select do |i|
           i.is_a? Libis::Ingester::Collection
