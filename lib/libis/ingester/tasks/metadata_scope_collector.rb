93b21044bef4d2a07d11f2dc03716979592fdaff
Kris Dekeyser <kris.dekeyser@libis.be>
2016-07-18 15:07:26 +0200

    Added Scope metadata collector

---
+++ lib/libis/ingester/tasks/metadata_scope_collector.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+
+require 'libis/ingester'
+require 'libis/services/scope/search'
+require 'libis/tools/metadata/dublin_core_record'
+
+require_relative 'metadata_collector'
+module Libis
+  module Ingester
+
+    class MetadataAlmaCollector < MetadataCollector
+
+      parameter host: nil,
+                description: 'URL of the Alma web service.'
+      parameter library: '32KUL_LIBIS_NETWORK',
+                description: 'SRU institution code'
+      parameter converter: nil
+
+      protected
+
+      def search(term)
+        unless @scope
+          @scope = Libis::Services::Scope::Search.new
+          @scope.connect(
+              Libis::Ingester::Config['scope_user'],
+              Libis::Ingester::Config['scope_passwd']
+          )
+        end
+        @scope.query(term)
+
+        @scope.next_record { |doc| return Libis::Tools::Metadata::DublinCoreRecord.new(doc.to_xml) }
+
+      rescue Exception => e
+        raise Libis::WorkflowError, "Scope request failed: #{e.message}"
+      end
+
+    end
+
+  end
+end
