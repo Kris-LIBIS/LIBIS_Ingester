03573bbfef0d464346a400c7930d0b730f39834d
Kris Dekeyser <kris.dekeyser@libis.be>
2016-08-23 10:25:06 +0200

    FIX ManifestationBuilder: typo

--- lib/libis/ingester/tasks/manifestation_builder.rb
+++ lib/libis/ingester/tasks/manifestation_builder.rb
@@ -306,7 +306,7 @@ module Libis
             [{}]
         end.each do |opts|
           opts = opts.dup
-          tgt_format = opts.delete(:target_format) || target_format
+          tgt_format = opts.delete(target_format) || target_format
           tgt_file = tempfile(src_file, tgt_format)
           temp_files << tgt_file
           tgt_file = tgt_file.path
