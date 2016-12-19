500b5c77773717a0f8dcc83c18911b1a1fe1f3c4
Kris Dekeyser <kris.dekeyser@libis.be>
2016-12-19 15:58:55 +0100

    LabelMapper: based on Base:Mapping

--- lib/libis/ingester/tasks/label_mapper.rb
+++ lib/libis/ingester/tasks/label_mapper.rb
@@ -1,68 +1,51 @@
-# encoding: utf-8
 require 'libis-ingester'
+require 'set'
 
 require_relative 'labeler'
-require_relative 'base/csv_mapping'
+require_relative 'base/mapping'
 module Libis
   module Ingester
 
     class LabelMapper < Libis::Ingester::Labeler
-      include Libis::Ingester::CsvMapping
+      include Libis::Ingester::Base::Mapping
 
-      parameter mapping_file: nil,
-                description: 'Path of mapping file.'
-      parameter mapping_format: 'csv',
-                description: 'Format in which the mapping file is written.',
-                constraint: %w'tsv csv xls'
-      parameter mapping_headers: %w'Name Thumbnail Label',
-                description: 'Headers for mapping file.'
-      parameter ignore_empty_label: false,
-                description: 'Ignore lines with empty label column.'
-      parameter lookup_field: 'Name',
-                description: 'The name of the lookup field in the mapping file.'
       parameter label_field: 'Label',
                 description: 'The name of the label field in the mapping file.'
-      parameter thumbnail_field: 'Thumbnail',
+      parameter thumbnail_field: nil,
                 description: 'The name of the thumbnail field in the mapping file.'
-
-      parameter recursive: true
-      parameter item_types: [Libis::Ingester::FileItem], frozen: true
-
 
       def apply_options(opts)
         super(opts)
-        options = {
-            file: parameter(:mapping_file),
-            values: parameter(:mapping_headers),
-            key: parameter(:lookup_field),
-            flags: [parameter(:thumbnail_field)],
-        }
-        options[:required] = [parameter(:label_field)] unless parameter(:ignore_empty_label)
-        case parameter(:mapping_format)
-          when 'csv'
-            options[:col_sep] = ','
-          when 'tsv'
-            options[:col_sep] = "\t"
-          else
-            # do nothing
+        set = Set.new(parameter(:mapping_headers))
+        set << parameter(:label_field)
+        parameter(:mapping_headers, set.to_a)
+        parameter(:required_fields, [parameter(:mapping_key), parameter(:label_field)])
+        if parameter(:thumbnail_field)
+          set = Set.new(parameter(:mapping_flags))
+          set << parameter(:thumbnail_field)
+          parameter(:mapping_flags, set.to_a)
         end
-        result = load_mapping(options)
-        @mapping = Hash[result[:mapping].map { |k, v| [k, v[parameter(:label_field)]] }]
-        @thumbnails = result[:flagged][parameter(:thumbnail_field)].flatten
       end
 
       protected
 
-      def mapping(name, item)
-        return name if @mapping.empty?
-        label = @mapping[name]
+      def get_label(name, item)
+        return name if self.mapping.empty?
+        label = self.lookup(name, parameter(:label_field))
         return label if label
         warn 'Could not find label in mapping table', item
         name
       end
 
       def thumbnail?(name)
-        @thumbnails.include?(name)
+        self.thumbnails.include?(name)
+      end
+
+      private
+
+      def thumbnails
+        return @thumbnails if @thumbnails
+        @thumbnails = self.flagged(parameter(:thumbnail_field)).flatten
       end
 
     end
