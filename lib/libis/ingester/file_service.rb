38cc6f5e0cf874f583168a2c6ac75b4e5d876b3b
Kris Dekeyser <kris.dekeyser@libis.be>
2017-02-06 17:24:48 +0100

    syntax error

--- lib/libis/ingester/file_service.rb
+++ lib/libis/ingester/file_service.rb
@@ -70,8 +70,8 @@ module Libis
 
       attr_accessor :root
 
-      def abspath(dir, file = '')
-        File.join(@root, dir, file)
+      def abspath(dir, file = nil)
+        file.nil? ? File.join(@root, dir) : File.join(@root, dir, file)
       end
 
       def relpath(path)
