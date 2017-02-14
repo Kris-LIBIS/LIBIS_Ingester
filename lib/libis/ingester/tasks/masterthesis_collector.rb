80617b721fdcebaa0666d8e940dfcb5f7458369f
Kris Dekeyser <kris.dekeyser@libis.be>
2017-02-14 16:12:02 +0100

    MasterthesisCollector: fixed public AR selection criteria.

--- lib/libis/ingester/tasks/masterthesis_collector.rb
+++ lib/libis/ingester/tasks/masterthesis_collector.rb
@@ -101,10 +101,10 @@ module Libis
 
         # Check AccessRight
         embargo = xml_doc['//embargo'].to_i
-        pub = xml_doc.embargo('isPubliek').blank?
+        pub = !(xml_doc.embargo('isPubliek').blank?)
         instelling_id = xml_doc['//instellingId'] || '50000050'
         # noinspection RubyNestedTernaryOperatorsInspection
-        ar_extension =  embargo == 0 ? (!pub.blank? ? 'PUBLIC' : 'IP-RESTRICTED') : 'PROTECTED'
+        ar_extension =  embargo == 0 ? (pub ? 'PUBLIC' : 'IP-RESTRICTED') : 'PROTECTED'
         ar_name = "AR_MT_#{instelling_id}_#{ar_extension}"
         unless Libis::Ingester::AccessRight.find_by(name: ar_name)
           raise Libis::WorkflowError, "AccessRight #{ar_name} not found."
