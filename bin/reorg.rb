f63c8e1529f4d30054a51adf61c605ddcfc28fdb
Kris Dekeyser <kris.dekeyser@libis.be>
2017-05-04 20:58:56 +0200

    Improved configuration persistence in reorg

--- bin/reorg.rb
+++ bin/reorg.rb
@@ -9,21 +9,10 @@ FILE_OPERATIONS = {
     link: 'linked'
 }
 
-######## Command-line
 config = nil
-x = read_config('')
-base_dir = x[:dir]
-parse_regex = x[:regex]
-path_expression = x[:expr]
-report_file = x[:report]
-file_operation = if x.has_key?(:move)
-                   x[:move] ? :move : :copy
-                 else
-                   x[:operation]
-                 end
-dummy_operation = nil
-interactive = false
-overwrite = false
+options = parse_config ''
+
+######## Command-line
 
 OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
@@ -32,55 +21,43 @@ OptionParser.new do |opts|
 
   opts.on('-c', '--config [STRING]', 'Configuration name') do |v|
     config = v
-    x = read_config(v)
-    base_dir = x[:dir]
-    parse_regex = x[:regex]
-    path_expression = x[:expr]
-    report_file = x[:report]
-    file_operation = if x.has_key?(:move)
-                       x[:move] ? :move : :copy
-                     else
-                       x[:operation]
-                     end
-    dummy_operation = nil
-    interactive = false
-    overwrite = false
+    options = parse_config config, options
   end
 
   opts.on('-i', '--[no-]interactive', 'Ask for action when changed files are found') do |v|
-    interactive = v
+    options[:interactive] = v
   end
 
   opts.on('-o', '--[no-]overwrite', 'Overwrite target if changed') do |v|
-    overwrite = v
+    options[:overwrite] = v
   end
 
   opts.on('--file-operation', [:move, :copy, :link], 'Operation to perform on files found') do |v|
-    file_operation = v
+    options[:file_operation] = v
   end
 
   opts.on('-b', '--base STRING', String, 'Directory that needs to be reorganized') do |v|
-    base_dir = v
+    options[:base_dir] = v
   end
 
   opts.on('-f', '--filter REGEX', Regexp, 'Regex for file name matching') do |v|
-    parse_regex = v
+    options[:parse_regex] = v
   end
 
   opts.on('-e', '--expression STRING', String, 'Path expression for new file path') do |v|
-    path_expression = v
+    options[:path_expression] = v
   end
 
   opts.on('--report FILE', String, 'Generate report in FILE') do |v|
-    report_file = v
+    options[:report_file] = v
   end
 
   opts.on('--no-report', 'Do not generate a report') do
-    report_file = false
+    options[:report_file] = false
   end
 
   opts.on('--dummy', 'Do not perform physical actions') do
-    dummy_operation = true
+    options[:dummy_operation] = true
   end
 
   opts.on('--unattended', 'Run without asking for input') do
@@ -90,26 +67,27 @@ OptionParser.new do |opts|
 end.parse!
 
 ######### Configuration
-config = get_config(config)
+config_new = get_config(config)
+options = parse_config config_new, options unless config_new == config
 
 ######### Source dir
-base_dir = get_base_dir(base_dir)
+options[:base_dir] = get_base_dir(options[:base_dir])
 
 ######### File REGEX
-parse_regex = get_parse_regex(parse_regex)
+options[:parse_regex] = get_parse_regex(options[:parse_regex])
 
 ######### Target expression
-path_expression = get_path_expression(path_expression)
-exit if path_expression.empty?
+options[:path_expression] = get_path_expression(options[:path_expression])
+exit if options[:path_expression].empty?
 
 ######### Report
-report_file = get_report_file(report_file)
+options[:report_file] = get_report_file(options[:report_file])
 
 ######### Dummy operations
-dummy_operation = get_dummy_operation(dummy_operation)
+options[:dummy_operation] = get_dummy_operation(options[:dummy_operation])
 
 ######### Move files
-file_operation = get_file_operation(file_operation)
+options[:file_operation] = get_file_operation(options[:file_operation])
 
 ######### Start
 puts
@@ -117,11 +95,12 @@ puts 'OK. We are all set. Starting to parse the files in the directory.'
 
 puts
 puts '========================================================================================='
-puts "Directory to reorganize: #{base_dir}"
-puts "Regex: #{parse_regex}"
-puts "Path expression: #{path_expression}"
-puts report_file ? "Creating report file #{report_file}" : 'Not creating a report'
-puts (dummy_operation ? 'Not p' : 'P') + 'erforming physical operations'
+puts "Directory to reorganize: #{options[:base_dir]}"
+puts "Regex: #{options[:parse_regex]}"
+puts "Path expression: #{options[:path_expression]}"
+puts "File operation: #{options[:file_operation]}"
+puts options[:report_file] ? "Creating report file #{options[:report_file]}" : 'Not creating a report'
+puts (options[:dummy_operation] ? 'Not p' : 'P') + 'erforming physical operations'
 puts '========================================================================================='
 puts
 exit unless @unattended || @hl.agree('Last chance to bail out. Continue?', true)
@@ -130,39 +109,39 @@ puts
 puts 'This can take a while. Please sit back and relax, grab a cup of coffee, have a quick nap or read a good book ...'
 
 # Save entries
-save_config(base_dir, parse_regex, path_expression, report_file, file_operation, config)
+save_config(options, config)
 
 # keeps track of folders created
 require 'set'
 target_dir_list = Set.new
 
-open_report(report_file)
+open_report(options[:report_file])
 
 require 'fileutils'
 count = {move: 0, duplicate: 0, update: 0, reject: 0, skipped_dir: 0, unmatched_file: 0}
 
-Dir.new(base_dir).entries.each do |file_name|
+Dir.new(options[:base_dir]).entries.each do |file_name|
   next if file_name =~/^\.\.?$/
-  entry = File.join(base_dir, file_name)
+  entry = File.join(options[:base_dir], file_name)
   unless File.file?(entry)
     puts "Skipping directory #{entry}." unless @report
     write_report(entry, '', '', 'Directory - skipped.')
     count[:skipped_dir] += 1
     next
   end
-  unless file_name =~ parse_regex
+  unless file_name =~ options[:parse_regex]
     puts "Skipping file #{file_name}. File name does not match expression." unless @report
     write_report(entry, '', '', 'Mismatch - skipped.')
     count[:unmatched_file] += 1
     next
   end
-  target = eval(path_expression)
+  target = eval(options[:path_expression])
   target_file = File.basename(target)
   target_dir = File.dirname(target)
-  target_dir = File.join(base_dir, target_dir) unless target_dir[0] == '/'
+  target_dir = File.join(options[:base_dir], target_dir) unless target_dir[0] == '/'
   unless target_dir_list.include?(target_dir)
     puts "-> Create directory '#{target_dir}'" unless @report
-    FileUtils.mkpath(target_dir) unless dummy_operation
+    FileUtils.mkpath(target_dir) unless options[:dummy_operation]
     target_dir_list << target_dir
   end
   target_path = File.join(target_dir, target_file)
@@ -176,7 +155,7 @@ Dir.new(base_dir).entries.each do |file_name|
     else
       # puts "source: #{File.mtime(entry)} #{'%11s' % Filesize.new(File.size(entry)).pretty} #{entry}"
       # puts "target: #{File.mtime(target_path)} #{'%11s' % Filesize.new(File.size(target_path)).pretty} #{target_path}"
-      if interactive ? @hl.agree('Overwrite target?') {|q| q.default = overwrite} : overwrite
+      if options[:interactive] ? @hl.agree('Overwrite target?') {|q| q.default = options[:overwrite]} : options[:overwrite]
         remark = 'Duplicate - updated'
         action = true
         count[:update] += 1
@@ -191,8 +170,8 @@ Dir.new(base_dir).entries.each do |file_name|
     count[:move] += 1
   end
   if action
-    puts "-> #{file_operation} '#{file_name}' to '#{target}'" unless @report
-    case file_operation
+    puts "-> #{options[:file_operation]} '#{file_name}' to '#{target}'" unless @report
+    case options[:file_operation]
       when :move
         FileUtils.move(entry, File.join(target_dir, target_file), force: true)
       when :copy
@@ -201,15 +180,15 @@ Dir.new(base_dir).entries.each do |file_name|
         FileUtils.symlink(entry, File.join(target_dir, target_file), force: true)
       else
         # Shouldn't happen
-        raise RuntimeError, "Bad file operation: '#{file_operation}'"
-    end unless dummy_operation
+        raise RuntimeError, "Bad file operation: '#{options[:file_operation]}'"
+    end unless options[:dummy_operation]
   end
   write_report(entry, target_dir, target_file, remark)
 end
 
 $stderr.puts "#{'%8d' % count[:skipped_dir]} dir(s) found and skipped."
 $stderr.puts "#{'%8d' % count[:unmatched_file]} file(s) found that did not match and skipped."
-$stderr.puts "#{'%8d' % count[:move]} file(s) #{FILE_OPERATIONS[file_operation]}."
+$stderr.puts "#{'%8d' % count[:move]} file(s) #{FILE_OPERATIONS[options[:file_operation]]}."
 $stderr.puts "#{'%8d' % count[:duplicate]} duplicate(s) found and skipped."
 $stderr.puts "#{'%8d' % count[:update]} changed file(s) found and updated."
 $stderr.puts "#{'%8d' % count[:reject]} changed file(s) found and rejected."
--- lib/libis/ingester/console/reorg_lib.rb
+++ lib/libis/ingester/console/reorg_lib.rb
@@ -10,7 +10,7 @@ def get_config(config)
     puts 'The configuration parameters you set are automatically retrieved and saved under that name.'
     puts 'These configurations exist:'
     puts_config
-    config = @hl.ask('Configuration to use.') { |q| q.default = config }
+    config = @hl.ask('Configuration to use.') {|q| q.default = config}
   end
   config
 end
@@ -29,7 +29,7 @@ def get_parse_regex(parse_regex)
     puts
     puts 'Now enter a regular expression that needs to be applied to each file in the directory.'
     puts 'Create groups for reference later in the directory structure to be created.'
-    parse_regex = @hl.ask('Enter REGEX: ') { |q| q.default = parse_regex }
+    parse_regex = @hl.ask('Enter REGEX: ') {|q| q.default = parse_regex}
   end
   Regexp.new(parse_regex)
 end
@@ -39,7 +39,7 @@ def get_path_expression(path_expression)
     puts
     puts 'Supply the relative or absolute path for each matching file (including file name).'
     puts 'Use $x for referencing the value of the the x-th group in the regex. "file_name" refers to the original file name.'
-    path_expression = @hl.ask('Enter path expression: ') { |q| q.default = path_expression }
+    path_expression = @hl.ask('Enter path expression: ') {|q| q.default = path_expression}
   end
   path_expression
 end
@@ -48,7 +48,7 @@ def get_report_file(report_file)
   if !@unattended || report_file.nil?
     puts
     puts 'Enter a file name for the report. Extension (csv/tsv/xml/yml) specifies the type. Type "-none-" for no report.'
-    report_file = @hl.ask('Report file name: ') { |q| q.default = report_file }
+    report_file = @hl.ask('Report file name: ') {|q| q.default = report_file}
   end
   report_file = nil if !report_file || report_file.empty? || report_file == '-none-'
   report_file
@@ -58,13 +58,14 @@ def get_file_operation(file_operation)
   if !@unattended || file_operation.nil?
     puts
     puts 'Which operation do you want to perform on the files?'
-    file_operation = @hl.ask('File operation? ', [:move, :copy, :link]) { |q| q.default = :move }
+    file_operation = @hl.ask('File operation? ', [:move, :copy, :link]) {|q| q.default = :move}
   end
   file_operation
 end
 
 def open_report(report_file)
   if report_file
+    # noinspection RubyStringKeysInHashInspection
     @report_type = {'.csv' => :csv, '.tsv' => :tsv, '.xml' => :xml, '.yml' => :yml}[File.extname(report_file)]
     unless @report_type
       puts "Unknown file type: #{File.extname(report_file)}"
@@ -141,17 +142,35 @@ def config_file(config)
 end
 
 def configurations
-  Dir.glob(File.join(ENV['HOME'], '.reorg*.data')).map { |x| x.scan(/reorg(.*).data$/).first.first rescue '' }
+  Dir.glob(File.join(ENV['HOME'], '.reorg*.data')).map {|x| x.scan(/reorg(.*).data$/).first.first rescue ''}
 end
 
-def save_config(base_dir, parse_regex, path_expression, report_file, file_operation, config)
+def save_config(options, config)
   File.open(config_file(config), 'w') do |f|
-    f.puts "dir: #{base_dir}"
-    f.puts "regex: #{parse_regex}"
-    f.puts "expr: #{path_expression}"
-    f.puts "report: #{report_file}"
-    f.puts "operation: #{file_operation}"
+    f.puts "dir: #{options[:base_dir]}"
+    f.puts "regex: #{options[:parse_regex]}"
+    f.puts "expr: #{options[:path_expression]}"
+    f.puts "report: #{options[:report_file]}"
+    f.puts "operation: #{options[:file_operation]}"
+    f.puts "interactive: #{options[:interactive]}"
+    f.puts "overwrite: #{options[:overwrite]}"
   end
+end
+
+def parse_config(config, options = {})
+  x = read_config(config)
+  options[:base_dir] = x[:dir]
+  options[:parse_regex] = x[:regex]
+  options[:path_expression] = x[:expr]
+  options[:report_file] = x[:report]
+  options[:file_operation] = if x.has_key?(:move)
+                               x[:move] ? :move : :copy
+                             else
+                               x[:operation]
+                             end
+  options[:interactive] = x[:interactive]
+  options[:overwrite] = x[:overwrite]
+  options
 end
 
 def read_config(config)
@@ -169,7 +188,7 @@ def puts_config
   configurations.each do |c|
     x = read_config(c)
     puts "- #{c}:"
-    x.each { |k, v| puts "   - #{k}: #{v}" }
+    x.each {|k, v| puts "   - #{k}: #{v}"}
   end
 end
 
