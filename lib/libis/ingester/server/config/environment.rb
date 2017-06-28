349aa6449b436dbd6e36efff82397234feb60c97
Kris Dekeyser <kris.dekeyser@libis.be>
2017-06-28 18:21:36 +0200

    API server cleanup

--- lib/libis/ingester/server/config/environment.rb
+++ lib/libis/ingester/server/config/environment.rb
@@ -6,6 +6,5 @@ require File.expand_path('../application', __FILE__)
 #
 # File.open(File.expand_path('../key.priv.pem', __FILE__), 'w') { |f| f.puts(RSA_PRIVATE.to_pem)}
 # File.open(File.expand_path('../key.pem', __FILE__), 'w') { |f| f.puts(RSA_PUBLIC.to_pem)}
-OpenSSL::PKey::RSA
 RSA_PRIVATE = OpenSSL::PKey::RSA.new File.read(File.expand_path('../key.priv.pem', __FILE__))
 RSA_PUBLIC = OpenSSL::PKey::RSA.new File.read(File.expand_path('../key.pem', __FILE__))
