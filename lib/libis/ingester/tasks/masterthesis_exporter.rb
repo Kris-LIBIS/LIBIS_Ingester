695ac647b4190428b31edd5818759de2f11fc891
Kris Dekeyser <kris.dekeyser@libis.be>
2018-01-30 13:28:19 +0100

    Catch failed to send email as warning message and continue.

--- lib/libis/ingester/tasks/masterthesis_exporter.rb
+++ lib/libis/ingester/tasks/masterthesis_exporter.rb
@@ -127,6 +127,8 @@ module Libis
         STR
         mail.deliver!
         debug "Report sent to #{parameter(:mail_to)}#{parameter(:mail_cc).blank? ? '' : " and #{parameter(:mail_cc)}"}."
+      rescue Net::TimeoutError => e
+        warn "Ingest report could not be sent by email. The exported XML files can be found at '#{parameter(:export_dir)}'."
       end
 
     end
