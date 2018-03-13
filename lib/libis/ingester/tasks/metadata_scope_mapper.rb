8850fb35a4a21deb528fbe73eb51666b6078f7e0
Kris Dekeyser <kris.dekeyser@libis.be>
2018-03-13 08:00:44 +0100

    Added MetadataScopeMapper for UA

---
+++ lib/libis/ingester/tasks/metadata_scope_mapper.rb
@@ -0,0 +1,30 @@
+require 'libis/ingester/tasks/base/mapping'
+
+require_relative 'metadata_scope_collector'
+module Libis
+  module Ingester
+
+    class MetadataAlmaMapper < MetadataScopeCollector
+      include Base::Mapping
+
+      parameter search_field: 'ScopeID',
+                description: 'Column name of the column in the mapping table that contains the search value.'
+
+      def apply_options(opts)
+        super(opts)
+        set = Set.new(parameter(:mapping_headers))
+        set << parameter(:search_field)
+        parameter(:mapping_headers, set.to_a)
+        parameter(:required_fields, [parameter(:mapping_key), parameter(:search_field)])
+      end
+
+      protected
+
+      def get_search_term(item)
+        lookup(super(item), parameter(:search_field))
+      end
+
+    end
+
+  end
+end
